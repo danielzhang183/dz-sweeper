dbee2ef5a47d80626565452bc843162f9af3f5a7
danielzhang183 <danielzhang182@gmail.com>
2022-07-07 03:11:20 +0800

    chore: finish complete func

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,10 +2,10 @@
 interface BlockState {
   x: number
   y: number
-  revealed?: boolean
+  revealed: boolean
   mine?: boolean
   flagged?: boolean
-  adjacentMines?: number
+  adjacentMines: number
 }
 
 const WIDTH = 10
@@ -14,51 +14,114 @@ const state = reactive(
   Array.from({ length: HEIGHT }, (_, y) =>
     Array.from({ length: WIDTH },
       (_, x): BlockState => ({
-        x, y,
+        x, y, adjacentMines: 0, revealed: false,
       })),
   ),
 )
 
-function generateMines() {
+function generateMines(initial: BlockState) {
   for (const row of state) {
-    for (const block of row)
-      block.mine = Math.random() < 0.3
+    for (const block of row) {
+      if (Math.abs(initial.x - block.x) < 1)
+        continue
+      if (Math.abs(initial.y - block.y) < 1)
+        continue
+      block.mine = Math.random() < 0.2
+    }
   }
+  updateNumbers()
 }
 
 const directions = [
-  []
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+]
+
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-orange-500',
+  'text-red-500',
+  'text-purple-500',
+  'text-pink-500',
+  'text-teal-500',
 ]
 
 function updateNumbers() {
   state.forEach((row, y) => {
     row.forEach((block, x) => {
       if (block.mine)
+        return
+      directions.forEach(([dx, dy]) => {
+        const x2 = x + dx
+        const y2 = y + dy
+        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+          return
+        if (state[y2][x2].mine)
+          block.adjacentMines += 1
+      })
     })
   })
 }
 
-function onClick(x: number, y: number) {
-  console.log(x, y)
+let mineGenerated = false
+const dev = true
+
+function onClick(block: BlockState) {
+  if (!mineGenerated) {
+    generateMines(block)
+    mineGenerated = true
+  }
+  block.revealed = true
+  if (block.mine)
+    alert('BOOm')
 }
 
-generateMines()
+function getBlockClass(block: BlockState) {
+  if (!block.revealed)
+    return 'bg-gray-500/10'
+  return block.mine ? 'bg-red-500/50' : numberColors[block.adjacentMines]
+}
 </script>
 
 <template>
   <div>
     MineSweeper
 
-    <div v-for="row, y in state" :key="y">
-      <button
-        v-for="item, x in row"
-        :key="x"
-        w-10 h-10
-        hover:bg-gray border
-        @click="onClick(x, y)"
+    <div p5>
+      <div
+        v-for="row, y in state"
+        :key="y"
+        flex="~"
+        items-center justify-center
       >
-        {{ item.mine ? '💣' : item.adjacentMines || '-' }}
-      </button>
+        <button
+          v-for="block, x in row"
+          :key="x"
+          flex="~"
+          items-center justify-center
+          w-10 h-10 m="0.5"
+          hover="bg-gray/10"
+          border="1 gray-400/10"
+          :class="getBlockClass(block)"
+          @click="onClick(block)"
+        >
+          <template v-if="block.revealed || dev">
+            <div v-if="block.mine" i-mdi:mine />
+            <div v-else>
+              {{ block.adjacentMines }}
+            </div>
+          </template>
+        </button>
+      </div>
     </div>
   </div>
 </template>
